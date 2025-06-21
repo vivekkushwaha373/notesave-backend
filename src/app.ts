c12596ffd75f36d4dd238761ba2cba0837507a4a
MyGit <vivekkushwaha734@gmail.com>
2025-06-22 02:24:13 +0530

    cors production fixing

--- src/app.ts
+++ src/app.ts
@@ -24,12 +24,12 @@ const corsOptions = {
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
 // Middleware
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+
 
 app.use(cookieParser());
 app.use(express.json());
